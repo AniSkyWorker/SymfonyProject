9f89e2ecb68573dfe52919ec88cccc448951b588
AniSkyWorker <holyrecycler@mail.ru>
2017-11-05 16:15:22 +0300

    поправил верстку в логинации

---
+++ src/AppBundle/Resources/config/doctrine/UserAccount.orm.php
@@ -0,0 +1,32 @@
+<?php
+
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+$metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_NONE);
+$metadata->customRepositoryClassName = 'AppBundle\Repository\UserAccountRepository';
+$metadata->setChangeTrackingPolicy(ClassMetadataInfo::CHANGETRACKING_DEFERRED_IMPLICIT);
+$metadata->mapField(array(
+   'fieldName' => 'id',
+   'type' => 'integer',
+   'id' => true,
+   'columnName' => 'id',
+  ));
+$metadata->mapField(array(
+   'columnName' => 'nickname',
+   'fieldName' => 'nickname',
+   'type' => 'string',
+   'length' => '55',
+  ));
+$metadata->mapField(array(
+   'columnName' => 'email',
+   'fieldName' => 'email',
+   'type' => 'string',
+   'length' => '55',
+  ));
+$metadata->mapField(array(
+   'columnName' => 'password',
+   'fieldName' => 'password',
+   'type' => 'string',
+   'length' => '55',
+  ));
+$metadata->setIdGeneratorType(ClassMetadataInfo::GENERATOR_TYPE_AUTO);
